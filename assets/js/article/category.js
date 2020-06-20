45850732849212a8512438750accf57fbe5606fb
liujinhu0511 <960079973@qq.com>
2020-06-20 23:01:40 +0800

    完成文章类别管理增删改查功能

--- assets/js/article/category.js
+++ assets/js/article/category.js
@@ -1,5 +1,6 @@
 $(function () {
-  var addIndex;
+  var addIndex, exitIndex;
+  var form = layui.form;
   // 获取文章信息,渲染列表
   renderCategory();
 
@@ -18,27 +19,86 @@ $(function () {
     addIndex = layer.open({
       type: 1,
       title: '添加文章分类',
-      content: $('#add-form').html(),
-      area: ['500px', '300px'],
+      content: $('#tpl-add').html(),
+      area: ['500px', '250px'],
     });
 
   })
+  // 点击编辑，显示弹出层
+  $('tbody').on('click', '#btn-exit', function () {
+    // dom方法,可以快速获取元素身上yidata开头的自定义属性值
+    // var id = e.target.dataset.id;
+    // var name = e.target.dataset.name;
+    // var alias = e.target.dataset.alias;
+    var data = JSON.parse(JSON.stringify(this.dataset));
+    exitIndex = layer.open({
+      type: 1,
+      title: '修改文章分类',
+      content: $('#tpl-exit').html(),
+      area: ['500px', '250px'],
+      success: function () {
+        // 这里的对象只能是字面量对象
+        form.val('exit-form', data)
+      }
+    });
 
+  })
   // 添加文章分类
-  $('body').on('submit', '#add-form', function (e) {
+  $('body').on('submit', '#category-list', function (e) {
     e.preventDefault();
-    alert('11');
-    // $.ajax({
-    //   type: 'POST',
-    //   url: '/my/article/addcates',
-    //   data: this.serialize(),
-    //   success: function (res) {
-    //     layer.msg(res.message);
-    //     if (res.status === 0) {
-    //       renderCategory();
-    //       layer.close(addIndex);
-    //     }
-    //   }
-    // })
+    $.ajax({
+      type: 'POST',
+      url: '/my/article/addcates',
+      data: $(this).serialize(),
+      success: function (res) {
+        layer.msg(res.message);
+        if (res.status === 0) {
+          renderCategory();
+          layer.close(addIndex);
+        }
+      }
+    })
+  })
+
+  // 修改文章列表
+  $('body').on('submit', '#exit-form', function (e) {
+    var data = $(this).serializeArray();
+    data[0].name = "Id";
+    e.preventDefault();
+    $.ajax({
+      type: 'POST',
+      url: '/my/article/updatecate',
+      data: data,
+      success: function (res) {
+        layer.msg(res.message);
+        if (res.status === 0) {
+          renderCategory();
+          layer.close(exitIndex);
+        }
+      }
+    })
+  })
+
+
+  // 删除列表信息
+  $('tbody').on('click', '#btn-del', function () {
+    // 获取id
+    var Id = $(this).attr('data-id');
+
+    layer.confirm('你确定要删除我吗？', {
+      icon: 3,
+      title: '提示'
+    }, function (index) {
+      $.ajax({
+        url: '/my/article/deletecate/' + Id,
+        success: function (res) {
+          layer.msg(res.message);
+          if (res.status === 0) {
+            renderCategory();
+          }
+        }
+      })
+    });
+
   })
 })
